730f936ee58e548968bd1f51eaf293c1035da26e
Halldór Stefánsson <halldors17@ru.is>
2018-10-25 15:52:04 +0000

    refactored board from object to class

--- src/logic/board.test.js
+++ src/logic/board.test.js
@@ -1,25 +1,31 @@
 const Board = require("./board");
 
 it("should return true for first row of x's", () => {
-    expect(Board.winCheck(['X', 'X', 'X', 'N', 'N', 'N', 'N', 'N', 'N'], 'X')).toBe(true);
+    const Board2 = new Board();
+    expect(Board2.winCheck(['X', 'X', 'X', 'N', 'N', 'N', 'N', 'N', 'N'], 'X')).toBe(true);
 });
 
 it("should return true for second row of o's", () => {
-    expect(Board.winCheck(['N', 'N', 'N', 'O', 'O', 'O', 'N', 'X', 'X'], 'O')).toBe(true);
+    const Board2 = new Board();
+    expect(Board2.winCheck(['N', 'N', 'N', 'O', 'O', 'O', 'N', 'X', 'X'], 'O')).toBe(true);
 });
 
 it("should return true for third row of o's", () => {
-    expect(Board.winCheck(['N', 'N', 'N', 'O', 'N', 'O', 'O', 'O', 'O'], 'O')).toBe(true);
+    const Board2 = new Board();
+    expect(Board2.winCheck(['N', 'N', 'N', 'O', 'N', 'O', 'O', 'O', 'O'], 'O')).toBe(true);
 });
 
 it("should return true for first column of x's", () => {
-    expect(Board.winCheck(['X', 'N', 'N', 'X', 'N', 'N', 'X', 'O', 'O'], 'X')).toBe(true);
+    const Board2 = new Board();
+    expect(Board2.winCheck(['X', 'N', 'N', 'X', 'N', 'N', 'X', 'O', 'O'], 'X')).toBe(true);
 });
 
 it("should return true for second column of x's", () => {
-    expect(Board.winCheck(['N', 'X', 'N', 'N', 'X', 'N', 'N', 'X', 'O'], 'X')).toBe(true);
+    const Board2 = new Board();
+    expect(Board2.winCheck(['N', 'X', 'N', 'N', 'X', 'N', 'N', 'X', 'O'], 'X')).toBe(true);
 });
 
 it("should return true for third column of x's", () => {
-    expect(Board.winCheck(['N', 'N', 'X', 'N', 'N', 'X', 'N', 'N', 'X'], 'X')).toBe(true);
+    const Board2 = new Board();
+    expect(Board2.winCheck(['N', 'N', 'X', 'N', 'N', 'X', 'N', 'N', 'X'], 'X')).toBe(true);
 });
