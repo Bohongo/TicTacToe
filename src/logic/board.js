f0b04dd62aa95bc18bf8d63d7c5573306b9caef6
Halldór Stefánsson <halldors17@ru.is>
2018-10-25 15:17:18 +0000

    board check for second row

--- src/logic/board.js
+++ src/logic/board.js
@@ -3,6 +3,9 @@ Board.winCheck = function(boardValues, letter) {
     if (boardValues[0] == letter && boardValues[1] == letter && boardValues[2] == letter) {
         return true;
     }
+    if (boardValues[3] == letter && boardValues[4] == letter && boardValues[5] == letter) {
+        return true;
+    }
     return false;
 }
 
--- src/logic/board.test.js
+++ src/logic/board.test.js
@@ -1,5 +1,9 @@
 const Board = require("./board");
 
-it("should return true", () => {
+it("should return true for first row of x's", () => {
     expect(Board.winCheck(['X', 'X', 'X', 'N', 'N', 'N', 'N', 'N', 'N'], 'X')).toBe(true);
+});
+
+it("should return true for second row of o's", () => {
+    expect(Board.winCheck(['N', 'N', 'N', 'O', 'O', 'O', 'N', 'X', 'X'], 'O')).toBe(true);
 });
