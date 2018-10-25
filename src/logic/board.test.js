35faff0bcf5e98d30f67adfbb5e34b342739b07c
GunnlaugurG <34029342+GunnlaugurG@users.noreply.github.com>
2018-10-25 18:50:59 +0000

    Refactored and added checkValidMove with tests

--- src/logic/board.test.js
+++ src/logic/board.test.js
@@ -2,40 +2,52 @@ const Board = require("./board");
 
 it("should return true for first row of x's", () => {
     const Board2 = new Board();
-    expect(Board2.winCheck(['X', 'X', 'X', 'N', 'N', 'N', 'N', 'N', 'N'], 'X')).toBe(true);
+    Board2.boardValues =['X', 'X', 'X', 'N', 'N', 'N', 'N', 'N', 'N'];
+    expect(Board2.winCheck('X')).toBe(true);
 });
 
 it("should return true for second row of o's", () => {
     const Board2 = new Board();
-    expect(Board2.winCheck(['N', 'N', 'N', 'O', 'O', 'O', 'N', 'X', 'X'], 'O')).toBe(true);
+    Board2.boardValues =['N', 'N', 'N', 'O', 'O', 'O', 'N', 'X', 'X'];
+    expect(Board2.winCheck('O')).toBe(true);
 });
 
 it("should return true for third row of o's", () => {
     const Board2 = new Board();
-    expect(Board2.winCheck(['N', 'N', 'N', 'O', 'N', 'O', 'O', 'O', 'O'], 'O')).toBe(true);
+    Board2.boardValues =['N', 'N', 'N', 'O', 'N', 'O', 'O', 'O', 'O'];
+    expect(Board2.winCheck('O')).toBe(true);
 });
 
 it("should return true for first column of x's", () => {
     const Board2 = new Board();
-    expect(Board2.winCheck(['X', 'N', 'N', 'X', 'N', 'N', 'X', 'O', 'O'], 'X')).toBe(true);
+    Board2.boardValues =['X', 'N', 'N', 'X', 'N', 'N', 'X', 'O', 'O'];
+    expect(Board2.winCheck('X')).toBe(true);
 });
 
 it("should return true for second column of x's", () => {
     const Board2 = new Board();
-    expect(Board2.winCheck(['N', 'X', 'N', 'N', 'X', 'N', 'N', 'X', 'O'], 'X')).toBe(true);
+    Board2.boardValues =['N', 'X', 'N', 'N', 'X', 'N', 'N', 'X', 'O'];
+    expect(Board2.winCheck('X')).toBe(true);
 });
 
 it("should return true for third column of x's", () => {
     const Board2 = new Board();
-    expect(Board2.winCheck(['N', 'N', 'X', 'N', 'N', 'X', 'N', 'N', 'X'], 'X')).toBe(true);
+    Board2.boardValues = ['N', 'N', 'X', 'N', 'N', 'X', 'N', 'N', 'X']
+    expect(Board2.winCheck('X')).toBe(true);
 });
 
 it("should return true for downward slope of x's", () => {
     const Board2 = new Board();
-    expect(Board2.winCheck(['X', 'N', 'N', 'N', 'X', 'N', 'N', 'N', 'X'], 'X')).toBe(true);
+    Board2.boardValues = ['X', 'N', 'N', 'N', 'X', 'N', 'N', 'N', 'X'];
+    expect(Board2.winCheck('X')).toBe(true);
 });
 
 it("should return true for upward slope of x's", () => {
     const Board2 = new Board();
-    expect(Board2.winCheck(['N', 'N', 'X', 'N', 'X', 'N', 'X', 'N', 'N'], 'X')).toBe(true);
+    Board2.boardValues =['N', 'N', 'X', 'N', 'X', 'N', 'X', 'N', 'N'];
+    expect(Board2.winCheck('X')).toBe(true);
+});
+it("Should return true because the whole array is set to N", () => {
+  const Board2 = new Board();
+  expect(Board2.checkValidMove(1)).toBe(true);
 });
