8978f7daa7cd5f816a3d9c2ef5490b0d19033c05
einarorri <einart16@ru.is>
2018-10-27 17:41:51 +0000

    added a reset board function and successfully tested it

--- src/server/api.js
+++ src/server/api.js
@@ -4,21 +4,28 @@ const Logic = require("../logic/gameLogic");
 
 
 router.get("/", (req, res) => {
-  res.status(405).send({ error: "GET method not allowed, try OPTIONS." });
+  res.status(405).send({
+    error: "GET method not allowed, try OPTIONS."
+  });
 });
 
 router.options("/", (req, res) => {
   const options = {
-    options: { get: ["/api/tictactoe", "/api/tictactoe/{NAME}"] }
+    options: {
+      get: ["/api/tictactoe", "/api/tictactoe/{NAME}"]
+    }
   };
   res.status(200).send(options);
 });
 
 router.get("/tictactoe/:box", (req, res) => {
-  var x = {Player: Logic};
+  var x = {
+    Player: Logic
+  };
   Logic.move(req.params.box);
   res.status(200).send(x);
 });
+
 
 module.exports = router;
 //module.exports = app;
