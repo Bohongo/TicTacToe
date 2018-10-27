4f156d4869d16dbbd067628e8e24d7dfade6885e
Elías Nökkvi Gíslason <eliasnokkvi@yahoo.com>
2018-10-27 17:49:02 +0000

    Changed the index.js file, cleaned up code and implemented with jquery

--- src/client/index.js
+++ src/client/index.js
@@ -1,15 +1,9 @@
-console.log("CONNECTED");
-document.getElementById('board').addEventListener('click', func);
-
-function func(e) {
-  var id = e.target.id;
-  console.log(e);
-  console.log('id: ' + id);
-  fetch('/api/tictactoe/' + id).then(function(res) {
-    console.log(res);
+var id = 0;
+$('.square').click(function() {
+id = $(this).attr('id');
+  fetch('/api/tictactoe/'+ id).then(function(res){
     return res.json();
-  }).then(function(data) {
-    console.log(data);
-    document.getElementById(id).innerHTML = data.Player.boardValues[id - 1];
+  }).then(function(data){
+      document.getElementById(id).innerHTML = data.Player.boardValues[id - 1];
   })
-};
+});
