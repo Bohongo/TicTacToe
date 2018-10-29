b34863eebb01c54c3523f7e9bda58ec26b7533be
Halldór Stefánsson <halldors17@ru.is>
2018-10-29 05:35:19 +0000

    small fix for puppeteer

--- puppeteer.test.js
+++ puppeteer.test.js
@@ -1,48 +1,39 @@
 const puppeteer = require('puppeteer');
 const APP = "https://bohongo.herokuapp.com/";
 
-let page;
-let browser;
 
-beforeEach(async () => {
-    browser = await puppeteer.launch({
-        headless: true,
-        slowMo: 0
+describe("puppeteer tests", () => {
+    let page;
+    let browser;
+
+    jest.setTimeout(15000);
+
+    beforeEach(async () => {
+        browser = await puppeteer.launch({
+            headless: true,
+            slowMo: 80
+        });
+        page = await browser.newPage();
     });
-    page = await browser.newPage();
-});
-afterEach(async () => {
-    await browser.close();
-});
 
+    afterEach(async () => {
+        await browser.close();
+    });
 
-describe("reset score", () => {
     test("Press the resetScores button", async () => {
         const response = await page.goto("https://bohongo.herokuapp.com/", {
             "waitUntil": "networkidle0"
         });
         await page.click('#resetScores');
         await page.waitFor(1000);
-    }, 20000);
-});
+    });
 
-describe("reset game", () => {
     test("Press the resetGame button", async () => {
         const response = await page.goto("https://bohongo.herokuapp.com/", {
             "waitUntil": "networkidle0"
         });
         await page.click('#resetGame');
         await page.waitFor(1000);
-    }, 20000);
-});
-/*
-describe("board test", () => {
-  test("Press square 1 in board", async () => {
-    await page.goto(APP, {
-      "waitUntil": "networkidle0"
     });
-    await page.click('1');
-    await page.waitFor(1000);
-  }, 20000);
 
-});*/
+});
